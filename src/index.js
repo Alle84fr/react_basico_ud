a0fb140cd8ff4b8e5c9651b3a223bddd42fb0ed5
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-10-14 20:51:01 -0300

    só mostrar como fazer alerta

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ import { createRoot } from 'react-dom/client';
 import Button from '../components/Button/indexBu';
 
 class App extends React.Component {
+
+  state = {
+    contador: 0,
+  }
+
+  //função - areo function - sem retorno
+  adicionar = () => {
+    alert("alerta");
+  }
+    
   render() {
     return (
       <div>
+
+        contador: {this.state.contador}
 
         <h1>Saluton React </h1>
 
 
 
-        <Button corTxt="#9d4edd">Default</Button>
-
-        <Button title="Prop /" decricao="valores na tag ">Propriedade</Button>
+        <Button press={this.adicionar}  corBtn="#9c6644"corTxt="#ede0d4">Default</Button>
 
       </div>
     );
